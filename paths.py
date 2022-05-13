e05a00ee70505d146ca0ed767e59098a918ad0eb
Ryan Anderson <ryan@thalana.co.za>
2022-05-13 10:59:00 +0100

    fix: correct paths

--- paths.py
+++ paths.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
-BASEDIR = Path("/home/ryan/Documents/Cambridge/MLMI/terms/project/experiments/VQA")
-VQA_API_DIR = BASEDIR / "PythonHelperTools/vqaTools"
-EVALUATION_API_DIR = BASEDIR / "PythonEvaluationTools/vqaEvaluation"
+BASEDIR = Path("/home/ryan/Documents/Cambridge/MLMI/terms/project/experiments/vision_encoders_vqa")
+VQA_API_BASE = Path("/home/ryan/Documents/Cambridge/MLMI/terms/project/experiments/VQA") 
+VQA_API_DIR = VQA_API_BASE / "PythonHelperTools/vqaTools"
+EVALUATION_API_DIR = VQA_API_DIR / "PythonEvaluationTools/vqaEvaluation"
