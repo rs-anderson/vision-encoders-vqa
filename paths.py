e9aed6a62a779b733e23dabb5cde4cd36da06ad8
rs-anderson <rsa39@cam.ac.uk>
2022-05-13 15:05:18 +0100

    add: ran inference on HPC on val

--- paths.py
+++ paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASEDIR = Path("/home/ryan/Documents/Cambridge/MLMI/terms/project/experiments/vision_encoders_vqa")
-VQA_API_BASE = Path("/home/ryan/Documents/Cambridge/MLMI/terms/project/experiments/VQA") 
+BASEDIR = Path("/home/rsa39/rds/rds-mlmi-2020-21-xyBFuSj0hm0/MLMI.2021-22/rsa39/project/vision-encoders-vqa")
+VQA_API_BASE = Path("/home/rsa39/rds/rds-mlmi-2020-21-xyBFuSj0hm0/MLMI.2021-22/rsa39/project/VQA") 
 VQA_API_DIR = VQA_API_BASE / "PythonHelperTools/vqaTools"
 EVALUATION_API_DIR = VQA_API_DIR / "PythonEvaluationTools/vqaEvaluation"
